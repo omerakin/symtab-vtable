797c3964ef244fb438028582fa25877f4477e2d7
omerakinusf <aakin@usfca.edu>
2017-03-05 20:42:10 -0800

    v7 -> Processing...

--- src/cs652/j/semantics/ComputeTypes.java
+++ src/cs652/j/semantics/ComputeTypes.java
@@ -4,6 +4,7 @@ import cs652.j.parser.JBaseListener;
 import cs652.j.parser.JParser;
 import org.antlr.symtab.*;
 import org.antlr.v4.runtime.ParserRuleContext;
+import sun.jvm.hotspot.types.Field;
 
 import java.util.HashMap;
 
@@ -23,97 +24,89 @@ public class ComputeTypes extends JBaseListener {
 		extendClasses = new HashMap<>();
 	}
 
-	@Override
+    @Override
+    public void exitFieldRef(JParser.FieldRefContext ctx) {
+        Type type = ctx.expression().type;
+        Symbol symbol = ((JClass) type).resolve(ctx.ID().getText());
+        String extendType = type.getName();
+        while (symbol == null) {
+            symbol = ((Scope) globalScope.resolve(extendClasses.get(extendType))).getSymbol(ctx.ID().getText());
+            extendType = extendClasses.get(extendType);
+        }
+        ctx.type = ((JField) symbol).getType();
+
+        buf.append(ctx.getText() + " is " + ctx.type.getName() + System.lineSeparator());
+    }
+
+    @Override
+    public void exitQMethodCall(JParser.QMethodCallContext ctx) {
+        Type type = ctx.expression().type;
+        Symbol symbol = ((JClass) type).resolve(ctx.ID().getText());
+        String extendType = type.getName();
+        while (symbol==null) {
+            symbol = ((Scope) globalScope.resolve(extendClasses.get(extendType))).getSymbol(ctx.ID().getText());
+            extendType = extendClasses.get(extendType);
+        }
+        ctx.type = ((JMethod) symbol).getType();
+
+        buf.append(ctx.getText() + " is " + ctx.type.getName() + System.lineSeparator());
+    }
+
+    @Override
+    public void enterCtorCall(JParser.CtorCallContext ctx) {
+        ctx.type = (Type) currentScope.resolve(ctx.ID().getText());
+    }
+
+    @Override
+    public void exitCtorCall(JParser.CtorCallContext ctx) {
+        buf.append(ctx.getText() + " is " + ctx.type.getName() + System.lineSeparator());
+    }
+
+    @Override
+    public void enterMethodCall(JParser.MethodCallContext ctx) {
+        Symbol symbol = currentScope.resolve(ctx.ID().getText());
+        if (symbol instanceof JMethod) {
+            ctx.type = ((JMethod) symbol).getType();
+        } else {
+            ParserRuleContext parserRuleContext = ctx.getParent();
+            while (!parserRuleContext.getClass().getSimpleName().contains("ClassDeclarationContext")) {
+                parserRuleContext = parserRuleContext.getParent();
+            }
+            String type = parserRuleContext.getChild(1).getText();
+            while (symbol==null && !extendClasses.isEmpty() && extendClasses.get(type)!=null) {
+                String extendType = extendClasses.get(type);
+                symbol =  ((Scope) globalScope.getSymbol(extendType)).getSymbol(ctx.ID().getText());
+                type =  extendType;
+            }
+            if (symbol instanceof JMethod) {
+                ctx.type = ((JMethod) symbol).getType();
+            }
+        }
+    }
+
+    @Override
+    public void exitMethodCall(JParser.MethodCallContext ctx) {
+        buf.append(ctx.getText() + " is " + ctx.type.getName() + System.lineSeparator());
+    }
+
+    @Override
+    public void enterThisRef(JParser.ThisRefContext ctx) {
+        Symbol symbol = currentScope.resolve(ctx.getText());
+        if (symbol instanceof JArg) { ctx.type = ((JArg) symbol).getType(); }
+    }
+
+    @Override
+    public void exitThisRef(JParser.ThisRefContext ctx) {
+        buf.append(ctx.getText() + " is " + ctx.type.getName() + System.lineSeparator());
+    }
+
+    @Override
 	public void enterLiteralRef(JParser.LiteralRefContext ctx) {
 		if (ctx.getText().contains(".")) { ctx.type = JFLOAT_TYPE;} else { ctx.type = JINT_TYPE; }
 	}
 
 	@Override
 	public void exitLiteralRef(JParser.LiteralRefContext ctx) {
-		buf.append(ctx.getText() + " is " + ctx.type.getName() + System.lineSeparator());
-	}
-
-	@Override
-	public void enterFieldRef(JParser.FieldRefContext ctx) {
-		String s2 = ctx.getText();
-		String[] string =ctx.getText().trim().split("\\.");
-		for (int i=0; i<string.length-1; i++) {
-			String s = string[i];
-			Symbol symbol;
-			if (i == 0) symbol = currentScope.resolve(s); else symbol = globalScope.resolve(ctx.type.getName());
-			if (symbol instanceof JArg || symbol instanceof JClass) {
-				String type;
-				if (symbol instanceof JArg) type = ((JArg) symbol).getType().getName(); else type = symbol.getName();
-				String ddd = string[i+1].replace("()","");
-				Symbol symbol1 = ((Scope) globalScope.getSymbol(type)).getSymbol(string[i+1].replace("()",""));
-				while (symbol1==null && !extendClasses.isEmpty() && extendClasses.get(type)!=null) {
-					String extendType = extendClasses.get(type);
-					symbol1 =  ((Scope) globalScope.getSymbol(extendType)).getSymbol(string[i+1].replace("()",""));
-					type =  extendType;
-				}
-				if (symbol1 instanceof JField) {
-					ctx.type = ((JField) symbol1).getType();
-				} else if (symbol1 instanceof JMethod) {
-					ctx.type = ((JMethod) symbol1).getType();
-				}
-			}
-		}
-	}
-
-	@Override
-	public void exitFieldRef(JParser.FieldRefContext ctx) {
-		buf.append(ctx.getText() + " is " + ctx.type.getName() + System.lineSeparator());
-	}
-
-	@Override
-	public void enterMethodCall(JParser.MethodCallContext ctx) {
-		Symbol symbol = currentScope.resolve(ctx.ID().getText());
-		if (symbol instanceof JMethod) {
-			ctx.type = ((JMethod) symbol).getType();
-		} else {
-			ParserRuleContext parserRuleContext = ctx.getParent();
-			while (!parserRuleContext.getClass().getSimpleName().contains("ClassDeclarationContext")) {
-				parserRuleContext = parserRuleContext.getParent();
-			}
-			String type = parserRuleContext.getChild(1).getText();
-			while (symbol==null && !extendClasses.isEmpty() && extendClasses.get(type)!=null) {
-				String extendType = extendClasses.get(type);
-				symbol =  ((Scope) globalScope.getSymbol(extendType)).getSymbol(ctx.ID().getText());
-				type =  extendType;
-			}
-			if (symbol instanceof JMethod) {
-				ctx.type = ((JMethod) symbol).getType();
-			}
-		}
-	}
-
-	@Override
-	public void exitMethodCall(JParser.MethodCallContext ctx) {
-		buf.append(ctx.getText() + " is " + ctx.type.getName() + System.lineSeparator());
-	}
-
-	@Override
-	public void enterQMethodCall(JParser.QMethodCallContext ctx) {
-		String s = ctx.getText();
-		Symbol symbol = currentScope.resolve(ctx.expression().getText());
-		if (symbol instanceof JArg) {
-			String type = ((JArg) symbol).getType().getName();
-			Symbol symbol1 = ((Scope) globalScope.getSymbol(type)).getSymbol(ctx.ID().getText());
-			while (symbol1==null && !extendClasses.isEmpty()) {
-				String extendType = extendClasses.get(type);
-				symbol1 =  ((Scope) globalScope.getSymbol(extendType)).getSymbol(ctx.ID().getText());
-				type =  extendType;
-			}
-			if (symbol1 instanceof JMethod) {
-				ctx.type = ((JMethod) symbol1).getType();
-			}
-		} else {
-			ctx.type = JINT_TYPE;
-		}
-	}
-
-	@Override
-	public void exitQMethodCall(JParser.QMethodCallContext ctx) {
 		buf.append(ctx.getText() + " is " + ctx.type.getName() + System.lineSeparator());
 	}
 
@@ -143,27 +136,6 @@ public class ComputeTypes extends JBaseListener {
 	@Override
 	public void exitIdRef(JParser.IdRefContext ctx) {
 		buf.append(ctx.ID().getText() + " is " + ctx.type.getName() + System.lineSeparator());
-	}
-
-	@Override
-	public void enterThisRef(JParser.ThisRefContext ctx) {
-		Symbol symbol = currentScope.resolve(ctx.getText());
-		if (symbol instanceof JArg) { ctx.type = ((JArg) symbol).getType(); }
-	}
-
-	@Override
-	public void exitThisRef(JParser.ThisRefContext ctx) {
-		buf.append(ctx.getText() + " is " + ctx.type.getName() + System.lineSeparator());
-	}
-
-	@Override
-	public void enterCtorCall(JParser.CtorCallContext ctx) {
-		ctx.type = (Type) currentScope.resolve(ctx.ID().getText());
-	}
-
-	@Override
-	public void exitCtorCall(JParser.CtorCallContext ctx) {
-		buf.append(ctx.getText() + " is " + ctx.type.getName() + System.lineSeparator());
 	}
 
 	@Override
